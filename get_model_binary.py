a27270d5519ad0f99e7b8dab154fbe3b821aec98
Hyunwook Jo <acerghjk@gmail.com>
2023-03-30 11:52:05 +0900

    Update get_model_binary.py

--- get_model_binary.py
+++ get_model_binary.py
@@ -9,9 +9,11 @@ parser.add_argument("--model_binary", default=None, type=str)
 parser.add_argument("--output_dir", default='kobart_summary', type=str)
 args = parser.parse_args()
 
-with open(args.hparams) as f:
-    hparams = yaml.load(f)
+with open(args.hparams, 'rb') as f:
+    hparams = yaml.full_load(f)
     
+
+
 inf = KoBARTConditionalGeneration.load_from_checkpoint(args.model_binary, hparams=hparams)
 
 inf.model.save_pretrained(args.output_dir)
